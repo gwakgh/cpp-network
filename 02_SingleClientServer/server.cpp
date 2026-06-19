82658859c2300e68f27a45fec808a0c77abd1998
gwakggh <gwakggh304@gmail.com>
2026-06-19 14:41:30 +0900

    02 싱글스레드 실습

---
+++ 02_SingleClientServer/server.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+void error_handling(const char* message);
+
+int main(int argc, char* argv[])
+{
+    int serv_sock;
+    int clnt_sock;
+
+    struct sockaddr_in serv_addr;
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size;
+
+    char message[1024];
+    int str_len;
+
+    if(argc != 2)
+    {
+        std::cerr << "Usage : " << argv[0] << " <port>" << std::endl;
+        exit(1);
+    }
+    
+    serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+    if(serv_sock == -1)
+    {
+        error_handling("socket() error");
+    }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(atoi(argv[1]));
+
+    if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+    {
+        error_handling("bind() error");
+    }
+
+    if(listen(serv_sock, 5) == -1)
+    {
+        error_handling("listen() error");
+    }
+
+    clnt_addr_size = sizeof(clnt_addr);
+    clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+    if(clnt_sock == -1)
+    {
+	    error_handling("accept() error");
+    }
+
+    while((str_len = read(clnt_sock, message, sizeof(message) - 1)) != 0)
+    {
+	    if(str_len == -1)	break;
+
+	    message[str_len] = '\0';
+	    std::cout << "Client Message: " << message << std::endl;
+
+	    write(clnt_sock, message, str_len);
+    }
+
+    std::cout << "Client Disconnected..." << std::endl;
+
+    close(clnt_sock);
+    close(serv_sock);
+    return 0;
+}
+
+void error_handling(const char* message)
+{
+    std::cerr << message << std::endl;
+    exit(1);
+}
